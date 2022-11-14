a3a8fe901a88378ed88177ec869f7a52bd68edef
LiatBrochman <92271841+LiatBrochman@users.noreply.github.com>
2022-11-14 12:13:19 +0200

    Order screen

---
+++ UberEatsUser/src/screens/OrderScreen/index.js
@@ -0,0 +1,23 @@
+import { Text, View, FlatList, StyleSheet } from 'react-native';
+import OrderListItem from '../../components/OrderListItem'
+import orders from '../../../assets/data/orders.json'
+const OrderScreen = () => {
+    return (
+    <View style={styles.container}>
+        <FlatList
+        data={orders}
+        renderItem={({item}) => <OrderListItem order={item} />}
+        />
+    </View>
+    );
+};
+
+export default OrderScreen;
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        width: "100%",
+        paddingTop:50
+    },
+})
