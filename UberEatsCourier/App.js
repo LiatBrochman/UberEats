715f0a7464c25306adbbe1c0909be23410c66122
LiatBrochman <92271841+LiatBrochman@users.noreply.github.com>
2022-11-15 16:29:39 +0200

    Order item

--- UberEatsCourier/App.js
+++ UberEatsCourier/App.js
@@ -1,32 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Image } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {FlatList, StyleSheet, View} from 'react-native';
 import orders from './assets/data/orders.json';
-import { Entypo } from "@expo/vector-icons"
+import OrderItem from "./src/components/OrderItem";
 
 const order = orders[0]
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-     <View>
-       <Image source={{uri: order.Restaurant.image}} style={{width: 50, height: 50}}/>
-       <Text>{order.Restaurant.name}</Text>
-       <Text>{order.Restaurant.address}</Text>
-       <Text>Delivery Details:</Text>
-       <Text>{order.User.name}</Text>
-       <Text>{order.User.address}</Text>
-       <Entypo name="check" size={30} color="black" />
-     </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <FlatList
+            data={orders}
+            renderItem={({item}) => <OrderItem order={item}/>}
+            />
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        paddingTop: 40
+    },
 });
