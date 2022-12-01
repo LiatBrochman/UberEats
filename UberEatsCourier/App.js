a330fef9f68ef42c4042e8c0c51fee0b21af9dbd
LiatBrochman <92271841+LiatBrochman@users.noreply.github.com>
2022-12-01 17:10:31 +0200

    Save courier to database

--- UberEatsCourier/App.js
+++ UberEatsCourier/App.js
@@ -1,16 +1,22 @@
 import {StatusBar} from "expo-status-bar";
-import {View, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
-import Navigation from './src/navigation'
+import {NavigationContainer} from '@react-navigation/native';
+import Navigation from './src/navigation';
+import {Amplify, Analytics} from 'aws-amplify';
+import {withAuthenticator} from 'aws-amplify-react-native'
+import awsconfig from './src/aws-exports';
+import AuthContextProvider from './src/contexts/AuthContext';
 
+Amplify.configure({...awsconfig, Analytics: {disabled: true,},});
 
-
-export default function App() {
+function App() {
     return (
         <NavigationContainer>
+            <AuthContextProvider>
                 <Navigation/>
+            </AuthContextProvider>
                 <StatusBar style="auto"/>
         </NavigationContainer>
     );
 };
 
+export default withAuthenticator(App);
--- UberEatsCourier/src/navigation/index.js
+++ UberEatsCourier/src/navigation/index.js
@@ -1,16 +1,26 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import OrdersScreen from '../screens/OrdersScreen';
 import OrdersDeliveryScreen from '../screens/OrdersDelivery';
+import ProfileScreen from '../screens/ProfileScreen';
+import {useAuthContext} from '../contexts/AuthContext';
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
+    const {dbCourier} = useAuthContext();
+
     return (
         <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen name="OrdersScreen" component={OrdersScreen}/>
-            <Stack.Screen name="OrdersDeliveryScreen" component={OrdersDeliveryScreen}/>
+            {dbCourier ? (
+                <>
+                    <Stack.Screen name="OrdersScreen" component={OrdersScreen}/>
+                    <Stack.Screen name="OrdersDeliveryScreen" component={OrdersDeliveryScreen}/>
+                </>
+            ) : (
+                <Stack.Screen name="Profile" component={ProfileScreen}/>
+            )}
         </Stack.Navigator>
-        )
+    )
 };
 
 export default Navigation
