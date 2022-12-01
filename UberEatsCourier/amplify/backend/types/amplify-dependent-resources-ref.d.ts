143c19e8aec5924edccb96241f1e8349f770074d
LiatBrochman <92271841+LiatBrochman@users.noreply.github.com>
2022-12-01 17:10:31 +0200

    Save courier to database

---
+++ UberEatsCourier/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,22 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "UberEats": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "UberEats": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
