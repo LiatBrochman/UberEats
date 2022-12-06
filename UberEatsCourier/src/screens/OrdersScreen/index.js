c2fcde47b39dbbbbf98184613ff86d4b5a3b087d
Joseph Shmuel <shmuelyos@gmail.com>
2022-12-06 16:33:03 +0200

    fixed coordination null error

--- UberEatsCourier/src/screens/OrdersScreen/index.js
+++ UberEatsCourier/src/screens/OrdersScreen/index.js
@@ -7,41 +7,47 @@ import OrderItem from '../../components/OrderItem';
 import MapView, {Marker} from "react-native-maps";
 import {Entypo} from "@expo/vector-icons";
 import {DataStore} from 'aws-amplify'
-import {Order} from '../../models'
+import {Order, Restaurant} from '../../models'
 
 
 const OrdersScreen = () => {
     const [orders, setOrders] = useState([])
+    const [restaurants, setRestaurants] = useState([])
+
     const bottomSheetRef = useRef(null);
     const {width, height} = useWindowDimensions();
 
     const snapPoints = useMemo(() => ["12%", "95%"], [])
 
     useEffect(() => {
-        DataStore.query(Order).then(setOrders);
+        DataStore.query(Order).then(setOrders)
+        DataStore.query(Restaurant).then(setRestaurants)
     }, [])
 
     return (
-        <GestureHandlerRootView style={{flex: 1, backgroundColor: 'lightblue'}}>
+        orders[0] && restaurants[0] && <GestureHandlerRootView style={{flex: 1, backgroundColor: 'lightblue'}}>
             <MapView
                 style={{height, width}}
                 showsUserLocation
                 followUserLocation
             >
-                {orders.map((order) => (
-                    <Marker
-                        key={order.id}
-                        title={order.Restaurant.name}
-                        description={order.Restaurant.address}
-                        coordinate={{
-                            latitude: order.Restaurant.lat,
-                            longitude: order.Restaurant.lng
-                        }}>
-                        <View style={{backgroundColor: 'green', padding: 5, borderRadius: 20}}>
-                            <Entypo name="shop" size={24} color="white"/>
-                        </View>
-                    </Marker>
-                ))}
+                {orders.map((order) => {
+                    const restaurant = restaurants.find(restaurant => restaurant.id === order.orderRestaurantId)
+                    return (
+                        <Marker
+                            key={order.id}
+                            title={restaurant.name}
+                            description={restaurant.address}
+                            coordinate={{
+                                latitude: restaurant.lat,
+                                longitude: restaurant.lng
+                            }}>
+                            <View style={{backgroundColor: 'green', padding: 5, borderRadius: 20}}>
+                                <Entypo name="shop" size={24} color="white"/>
+                            </View>
+                        </Marker>
+                    )
+                })}
 
             </MapView>
             <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints}>
