e96e7d41071702430e72d99f396707756a691d06
LiatBrochman <92271841+LiatBrochman@users.noreply.github.com>
2022-12-11 12:07:06 +0200

    Refactor order context and details

--- UberEatsCourier/src/contexts/OrderContext.js
+++ UberEatsCourier/src/contexts/OrderContext.js
@@ -1,13 +1,37 @@
 import {createContext, useState, useEffect, useContext} from "react";
 import {Auth, DataStore} from "aws-amplify";
-import {Courier, Order} from "../models";
+import {Courier, Order, OrderDish, Restaurant, User} from "../models";
 import {useAuthContext} from "./AuthContext";
+import {useRoute} from "@react-navigation/native";
 
 const OrderContext = createContext({});
 
 const OrderContextProvider = ({children}) => {
     const {dbCourier} = useAuthContext();
-    const {activeOrder, setActiveOrder} = useState();
+    const [order, setOrder] = useState();
+    const [user, setUser] = useState();
+    const [restaurant, setRestaurant] = useState(null);
+    const [orderDishes, setOrderDishes] = useState([]);
+    //const [dishes, setDishes] = useState([]);
+    // const route = useRoute();
+    // const id = route.params?.id;
+
+    const fetchOrder = (id) => {
+        if (id) {
+         //const fetchedOrder = await DataStore.query(Order, id);
+        DataStore.query(Order, id).then(setOrder)
+        }
+        //setOrder(fetchedOrder);
+    }
+
+    const fetchUser = (order) => {
+            if (order) {
+                DataStore.query(User, order.userID).then(setUser)
+                DataStore.query(Restaurant, order.orderRestaurantId).then(setRestaurant)
+                DataStore.query(OrderDish, od => od.orderID.eq(order.id)).then(setOrderDishes)
+            }
+      //  setUser(fetchedUser);
+    }
 
     const acceptOrder = (order) => {
 // updated the order, and change status, and assign the courier
@@ -21,7 +45,7 @@ const OrderContextProvider = ({children}) => {
     }
 
     return (
-        <OrderContext.Provider value={{acceptOrder}}>
+        <OrderContext.Provider value={{acceptOrder, order, user, fetchOrder, fetchUser, restaurant, orderDishes}}>
             {children}
         </OrderContext.Provider>
     )
