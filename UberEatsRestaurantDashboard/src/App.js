11623285c9cf036c4002521dcb4cff2932eacbef
Joseph Shmuel <shmuelyos@gmail.com>
2023-03-17 00:46:30 +0200

    ...

--- UberEatsRestaurantDashboard/src/App.js
+++ UberEatsRestaurantDashboard/src/App.js
@@ -4,11 +4,6 @@ import {withAuthenticator} from '@aws-amplify/ui-react';
 import ProtectedRoutes from "./routes/ProtectedRoutes";
 import './App.css'
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
-import {Amplify,Storage} from "aws-amplify";
-
-Amplify.configure({...awsExports, Analytics: {disabled: true}});
-Storage.configure({ region: awsExports.aws_project_region });
 
 const {Content} = Layout;
 
