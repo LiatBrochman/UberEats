cb5b4d7d07521cfaee624173aa691f269aa5a2a7
Liat Brochman <92271841+LiatBrochman@users.noreply.github.com>
2023-05-14 20:28:36 +0300

    Update globalFunctions.js
    ...

--- UberEatsRestaurantDashboard/src/myExternalLibrary/globalFunctions.js
+++ UberEatsRestaurantDashboard/src/myExternalLibrary/globalFunctions.js
@@ -1,29 +1,4 @@
-export function compareArrays(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        return true;
-    }
 
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-export function cleanUp() {
-
-    //un-subscribe from all
-    for (const key in subscription) {
-        // Check if the current object value has an "unsubscribe" method
-        if (typeof subscription[key].unsubscribe === 'function') {
-            // Call the "unsubscribe" method
-            // console.log("\n\n ~~~~~~~~~~~~~~~~~~~~~ unsubscribing from ~~~~~~~~~~~~~~~~~~~~~ :", JSON.stringify(subscription[key], null, 4))
-            window.subscription[key].unsubscribe()
-        }
-    }
-}
 
 export async function executeFunctionsSequentially(functions) {
     const results = [];
